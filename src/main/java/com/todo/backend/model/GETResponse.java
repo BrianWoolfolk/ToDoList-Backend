57b64d245aa1e9a078cbbf1060aaf0530d3feddd
Brian Woolfolk <brian.woolfolk@encora.com>
2024-10-23 12:38:00 -0700

    chore: Add Spring Boot Web dependency and refactor GETResponse package structure

--- src/main/java/com/todo/backend/model/GETResponse.java
+++ src/main/java/com/todo/backend/model/GETResponse.java
@@ -1,45 +1,20 @@
-package com.todo.backend;
+package com.todo.backend.model;
 
 import java.util.List;
 
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.todo.backend.Metrics.LastMetrics;
-import com.todo.backend.model.ToDo;
+import com.todo.backend.model.Metrics.LastMetrics;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @ResponseBody
+@Getter
+@RequiredArgsConstructor
 public class GETResponse {
-    // #region ################################ PROPERTIES
     final private List<ToDo> data;
     final private int page;
     final private int maxpage;
     final private LastMetrics metrics;
-    // #endregion
-
-    // #region ################################ CONSTRUCTOR
-    public GETResponse(List<ToDo> data, int page, int maxpage, LastMetrics mLastMetrics) {
-        this.data = data;
-        this.page = page;
-        this.maxpage = maxpage;
-        this.metrics = mLastMetrics;
-    }
-    // #endregion
-
-    // #region ################################ GETTERS
-    public List<ToDo> getData() {
-        return data;
-    }
-
-    public int getMaxpage() {
-        return maxpage;
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public LastMetrics getLastMetrics() {
-        return metrics;
-    }
-    // #endregion
 }
